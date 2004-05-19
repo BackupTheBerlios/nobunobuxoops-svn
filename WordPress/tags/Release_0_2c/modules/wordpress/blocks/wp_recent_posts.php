99dedf1ee98915fa2885cbfa8fe55266b05ef1d6
(no author) <(no author)@e5e8bf75-b47f-4d42-856e-453524dda3ac>
2004-05-19 14:04:36 +0000

    This commit was manufactured by cvs2svn to create tag 'XOOPS_MOD_0_2c'.

---
+++ WordPress/tags/Release_0_2c/modules/wordpress/blocks/wp_recent_posts.php
@@ -0,0 +1,28 @@
+<?
+function b_wp_recent_posts_show($options)	
+{
+	$id=1;
+	global $dateformat, $time_difference, $siteurl, $blogfilename;
+	global $tablelinks,$tablelinkcategories;
+	global $querystring_start, $querystring_equal, $querystring_separator, $month, $wpdb, $start_of_week;
+	global $tableposts,$tablepost2cat,$tablecomments,$tablecategories;
+	global $smilies_directory, $use_smilies, $wp_smiliessearch, $wp_smiliesreplace;
+	global $wp_bbcode, $use_bbcode, $wp_gmcode, $use_gmcode, $use_htmltrans, $wp_htmltrans, $wp_htmltranswinuni;
+	require_once(dirname(__FILE__).'/../wp-blog-header.php');
+	$no_posts = 10;
+	$skip_posts = 0;
+	$now = date('Y-m-d H:i:s',(time() + ($time_difference * 3600)));
+	$request = "SELECT * FROM $tableposts WHERE post_status = 'publish' ";
+	$request .= " AND post_date <= '".$now."'";
+	$request .= " ORDER BY post_date DESC LIMIT $skip_posts, $no_posts";
+	$lposts = $wpdb->get_results($request);
+	$output = '';
+	foreach ($lposts as $lpost) {
+		$post_title = stripslashes($lpost->post_title);
+		$permalink = get_permalink($lpost->ID);
+		$output .= '<li style="font-size:90%"><a href="' . $permalink . '" rel="bookmark" title="Permanent Link: ' . $post_title . '">' . $post_title . '</a></li>';
+	}
+	$block['content'] = $output;
+	return $block;
+}
+?>
