86de6f7fe48949a90823c84adfd89d9de942c036
(no author) <(no author)@e5e8bf75-b47f-4d42-856e-453524dda3ac>
2004-05-23 14:10:09 +0000

    This commit was manufactured by cvs2svn to create tag 'XOOPS_MOD_0_2d'.

---
+++ WordPress/tags/Release_0_2d/modules/wordpress/wp-config.php
@@ -0,0 +1,56 @@
+<?php
+define('ABSPATH', dirname(__FILE__).'/');
+include_once ABSPATH.'../../mainfile.php';
+/** WordPress's config file **/
+/** http://wordpress.org/   **/
+/** http://wordpress.xwd.jp/   **/
+
+// It is influenced by environment.
+mb_language("Japanese");
+mb_internal_encoding("euc-jp");
+
+// ** MySQL settings ** //
+define('WP_DB_NAME', XOOPS_DB_NAME);      // データベース名
+define('WP_DB_USER', XOOPS_DB_USER);      // データベースのユーザー名
+define('WP_DB_PASSWORD', XOOPS_DB_PASS);  // データベースパスワード
+define('WP_DB_HOST', XOOPS_DB_HOST);       // 99% このままでOK
+
+// Change the prefix if you want to have multiple blogs in a single database.
+global $xoopsDB,$xoopsUser,$wpdb;
+
+
+$table_prefix  = $xoopsDB->prefix('wp_');   // example: 'wp_' or 'b2' or 'mylogin_'
+
+$server = WP_DB_HOST;
+$loginsql = WP_DB_USER;
+$passsql = WP_DB_PASSWORD;
+$base = WP_DB_NAME;
+
+$my_pingserver[0]['server']="rpc.weblogs.com";
+$my_pingserver[0]['path']="/RPC2";
+$my_pingserver[0]['port']=80;
+//$my_pingserver[1]['server']="ping.bloggers.jp";
+//$my_pingserver[1]['path']="/rpc/";
+//$my_pingserver[1]['port']=80;
+//$my_pingserver[2]['server']="ping.myblog.jp";
+//$my_pingserver[2]['path']="/";
+//$my_pingserver[2]['port']=80;
+//$my_pingserver[3]['server']="bulkfeeds.net";
+//$my_pingserver[3]['path']="/rpc";
+//$my_pingserver[3]['port']=80;
+
+$wp_use_spaw=true;
+
+// Get everything else
+require_once(ABSPATH.'wp-settings.php');
+
+// Language File - example: 'wp-lang/lang_en.php'
+require_once(ABSPATH.'wp-lang/lang_ja.php');
+
+/* Stop editing */
+if (get_xoops_option('wordpress','wp_use_spaw') == 1) {
+	$wp_use_spaw=true;
+} else {
+	$wp_use_spaw=false;
+}
+?>
---
+++ WordPress/tags/Release_0_2d/modules/wordpress/xoops_install.php
@@ -0,0 +1,44 @@
+<?PHP
+function xoops_module_install_WordPress(&$module)
+{
+	global $xoopsDB, $xoopsUser, $xoopsConfig;
+	$db =& Database::getInstance();
+
+	$url = XOOPS_URL."/modules/".$module->getVar('dirname');
+    $query= "UPDATE ".$db->prefix("wp_options")." set option_value='$url' where option_id=1"; //siteurl
+    $q = $db->query($query);
+    $query= "UPDATE ".$db->prefix("wp_options")." set option_value='$url' where option_id=28"; //blodotgsping_url
+    $q = $db->query($query);
+    $query= "UPDATE ".$db->prefix("wp_options")." set option_value='$url/wp-images/smilies' where option_id=17"; //smilies_directory
+    $q = $db->query($query);
+
+	$now = date('Y-m-d H:i:s');
+	$query = "INSERT INTO ".$db->prefix("wp_posts")." (post_author, post_date, post_content, post_title, post_category) VALUES ('".$xoopsUser->uid()."', '$now', "._MI_WORDPRESS_INST_POST_CONTENT.", "._MI_WORDPRESS_INST_POST_TITLE.", '1')";
+
+	$q = $db->query($query);
+
+	$query = "INSERT INTO ".$db->prefix("wp_comments")." (comment_post_ID, comment_author, comment_author_email, comment_author_url, comment_author_IP, comment_date, comment_content) VALUES ('1', 'NobuNobu Xoops', 'mail@example.com', 'http://www.kowa.org/', '127.0.0.1', '$now',"._MI_WORDPRESS_INST_COMMENT_CONTENT.")";
+
+	$q = $db->query($query);
+
+	$query = "INSERT INTO ".$db->prefix("wp_options")." (`option_id`, `blog_id`, `option_name`, `option_can_override`, `option_type`, `option_value`, `option_width`, `option_height`, `option_description`, `option_admin_level`) VALUES (52, 0, 'date_format', 'Y', 3, "._MI_WORDPRESS_INST_OPTIONS_52.", 20, 8, '_LANG_INST_BASE_VALUE52', 4)";
+
+	$q = $db->query($query);
+	
+	$query = "INSERT INTO ".$db->prefix("wp_options")." (`option_id`, `blog_id`, `option_name`, `option_can_override`, `option_type`, `option_value`, `option_width`, `option_height`, `option_description`, `option_admin_level`) VALUES (53, 0, 'time_format', 'Y', 3, "._MI_WORDPRESS_INST_OPTIONS_53.", 20, 8, '_LANG_INST_BASE_VALUE53', 4)";
+
+	$q = $db->query($query);
+	
+	$query = "INSERT INTO ".$db->prefix("wp_optionvalues")." (`option_id`, `optionvalue`, `optionvalue_desc`, `optionvalue_max`, `optionvalue_min`, `optionvalue_seq`) VALUES (59, 'SELECT cat_id AS value, cat_name AS label FROM ".$db->prefix("wp_categories")." order by cat_name', '', NULL, NULL, 1)";
+
+	$q = $db->query($query);
+	
+	$level = 10;
+	$uname = $xoopsDB->quoteString($xoopsUser->getVar('uname'));
+	$email = $xoopsDB->quoteString($xoopsUser->getVar('email'));
+	$sql = "INSERT INTO ".$db->prefix("wp_users")." (ID, user_login,user_nickname,user_email, user_level,user_idmode) values(".$xoopsUser->uid().", $uname , $uname , $email , $level, 'nickname' )";
+	$q = $db->query($sql);
+
+	return true;
+}
+?>
