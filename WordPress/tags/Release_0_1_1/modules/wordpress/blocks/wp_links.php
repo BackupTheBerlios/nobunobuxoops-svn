bd7a928beae264e9a00e6861b5c33975531fca97
(no author) <(no author)@e5e8bf75-b47f-4d42-856e-453524dda3ac>
2004-05-28 15:34:18 +0000

    This commit was manufactured by cvs2svn to create tag 'XOOPS_MOD-0_1_1'.

---
+++ WordPress/tags/Release_0_1_1/modules/wordpress/blocks/wp_links.php
@@ -0,0 +1,18 @@
+<?php
+function b_wp_links_show($option)
+{
+	$id=1;
+	global $dateformat, $time_difference, $siteurl, $blogfilename;
+	global $tablelinks,$tablelinkcategories;
+    global $querystring_start, $querystring_equal, $querystring_separator, $month, $wpdb, $start_of_week;
+	global $tableposts,$tablepost2cat,$tablecomments,$tablecategories;
+	global $smilies_directory, $use_smilies, $wp_smiliessearch, $wp_smiliesreplace;
+	global $wp_bbcode, $use_bbcode, $wp_gmcode, $use_gmcode, $use_htmltrans, $wp_htmltrans, $wp_htmltranswinuni;
+	require_once(dirname(__FILE__).'/../wp-blog-header.php');
+	ob_start();
+	get_links_list();
+	$block['content'] = ob_get_contents();
+	ob_end_clean();
+	return $block;
+}
+?>
