6f491d46cf27861462bbe365c815d4f2478e74ef
(no author) <(no author)@e5e8bf75-b47f-4d42-856e-453524dda3ac>
2004-06-02 14:04:49 +0000

    This commit was manufactured by cvs2svn to create tag 'XOOPS_MOD-0_1_2'.

---
+++ WordPress/tags/Release_0_1_2/modules/wordpress/language/japanese/modinfo.php
@@ -0,0 +1,60 @@
+<?php
+// Module Info
+
+// The name of this module
+define("_MI_WORDPRESS_NAME","WordPress");
+
+// A brief description of this module
+define("_MI_WORDPRESS_DESC","WordPress MEのXOOPSモジュールです。");
+define("_MI_WORDPRESS_AUTHOR",'<a href="http://www.kowa.org/" target="_blank">のぶのぶ</a>');
+
+// Sub menu titles
+define("_MI_WORDPRESS_SMNAME1","Blogを書く");
+// Sample Blog Message
+define("_MI_WORDPRESS_INST_POST_CONTENT","'WordPress ME for Xoops2の導入成功おめでとうございます。<br /> . This is the first post. Edit or delete it, then start blogging!'");
+define("_MI_WORDPRESS_INST_POST_TITLE","'ようこそ、WordPressの世界へ！'");
+// Sample Comment
+define("_MI_WORDPRESS_INST_COMMENT_CONTENT"," 'コメントのサンプルです！<br />To delete a comment, just log in, and view the posts\' comments, there you will have the option to edit or delete them.'");
+// WordPress OptionTable Values
+define("_MI_WORDPRESS_INST_OPTIONS_22","'ja'");
+define("_MI_WORDPRESS_INST_OPTIONS_52","'Y年n月j日(l)'");
+define("_MI_WORDPRESS_INST_OPTIONS_53","'H時i分s秒'");
+// Config titles
+define("_MI_WPUSESPAW_CFG_MSG","投稿フォームにSPAWを使用する");
+define("_MI_WPUSESPAW_CFG_DESC","投稿フォームにSPAWを使用する場合にはYESを指定します");
+
+define("_MI_WPEDITAUTHGRP_CFG_MSG","初期権限が投稿権限をもつグループ");
+define("_MI_WPEDITAUTHGRP_CFG_DESC","ユーザ登録時に投稿権限(WordPressのユーザレベル１）のをもつグループ");
+
+define("_MI_WPADMINAUTHGRP_CFG_MSG","初期権限が管理権限をもつグループ");
+define("_MI_WPADMINAUTHGRP_CFG_DESC","ユーザ登録時に管理権限(WordPressのユーザレベル１０）のをもつグループ");
+
+define("_MI_WP_USE_XOOPS_SMILE","XOOPSのスマイルアイコンを使用");
+define("_MI_WP_USE_XOOPS_SMILE_DESC","スマイルアイコンをXOOPSで使用しているものに置き換える。");
+// Block Name
+define("_MI_WORDPRESS_BNAME1","WordPress カレンダー");
+define("_MI_WORDPRESS_BDESC1","WordPress カレンダー");
+
+define("_MI_WORDPRESS_BNAME2","WordPress 月別過去ログ");
+define("_MI_WORDPRESS_BDESC2","WordPress 月別過去ログ");
+
+define("_MI_WORDPRESS_BNAME3","WordPress カテゴリ一覧");
+define("_MI_WORDPRESS_BDESC3","WordPress カテゴリ一覧");
+
+define("_MI_WORDPRESS_BNAME4","WordPress Link");
+define("_MI_WORDPRESS_BDESC4","WordPress Link");
+
+define("_MI_WORDPRESS_BNAME5","WordPress 検索");
+define("_MI_WORDPRESS_BDESC5","WordPress 検索");
+
+define("_MI_WORDPRESS_BNAME6","WordPress 最近の投稿");
+define("_MI_WORDPRESS_BDESC6","WordPress 最近の投稿");
+
+define("_MI_WORDPRESS_BNAME7","WordPress 最近のコメント");
+define("_MI_WORDPRESS_BDESC7","WordPress 最近のコメント");
+
+define("_MI_WORDPRESS_BNAME8","WordPress 記事ブロック");
+define("_MI_WORDPRESS_BDESC8","WordPress 記事ブロック");
+
+define("_MI_WORDPRESS_AD_MENU1","WordPressオプション");
+?>
