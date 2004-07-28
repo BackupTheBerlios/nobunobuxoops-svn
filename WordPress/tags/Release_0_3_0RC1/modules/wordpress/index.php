02143fe38c6ab9fd46d5cfe869c72ac991bbca43
(no author) <(no author)@e5e8bf75-b47f-4d42-856e-453524dda3ac>
2004-07-28 04:14:15 +0000

    This commit was manufactured by cvs2svn to create tag 'XOOPS_MOD-0_3_0RC1'.

---
+++ WordPress/tags/Release_0_3_0RC1/modules/wordpress/index.php
@@ -0,0 +1,15 @@
+<?php 
+/* Don't remove these lines. */
+$blog = 1;
+include("header.php");
+wp_head();
+// Uncomment the next line if you want to track blog updates from weblogs.com
+//include_once(ABSPATH.WPINC.'/links-update-xml.php');
+if (file_exists(ABSPATH.'/themes/'.$xoopsConfig['theme_set'].'/index-template.php')) {
+	$themes = $xoopsConfig['theme_set'];
+} else {
+	$themes = "default";
+}
+include(ABSPATH.'/themes/'.$themes.'/index-template.php');
+include(XOOPS_ROOT_PATH."/footer.php");
+?>
